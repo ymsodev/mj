b6b7c5afe47fc4d6aae2d7bf397e63ac32ed3ee2
ymsodev <ymsodev@gmail.com>
2024-01-29 18:19:33 -0800

    Initial commit

---
+++ MicroJournal/App.cs
@@ -0,0 +1,96 @@
+using System.Diagnostics;
+using System.Text;
+using Microsoft.Data.Sqlite;
+
+namespace MicroJournal;
+
+internal static class App
+{
+	private static string TEMP_ENTRY_BUFFER_FILE = "MJ_NEW_ENTRY";
+	
+	public static void CreateNewEntry(string editor, string dataPath)
+	{
+		string? entryBufferPath = CreateEntryBuffer();
+		if (entryBufferPath == null)
+			return;
+
+		string? content = GetEntryContent(editor, entryBufferPath);
+		if (content == null)
+            return;
+
+		InsertEntry(dataPath, content);
+    }
+
+    private static string? CreateEntryBuffer()
+	{
+        string filename = $"{TEMP_ENTRY_BUFFER_FILE}_{Guid.NewGuid()}";
+        string entryBufferPath = Path.Combine(Path.GetTempPath(), filename);
+		try
+		{
+            using (FileStream fs = File.Open(entryBufferPath, FileMode.Create, FileAccess.ReadWrite, FileShare.None))
+            using (StreamWriter sw = new(fs))
+            {
+                sw.WriteLine();
+                sw.WriteLine(@"// This is a buffer for a new micro-journal entry.");
+                sw.WriteLine(@"// The lines that start with '//' will be ignored.");
+            }
+        }
+		catch (Exception e)
+		{
+			Console.Error.WriteLine($"Failed to create an entry buffer: {e.Message}");
+			return null;
+		}
+        return entryBufferPath;
+    }
+
+	private static string? GetEntryContent(string editor, string entryBufferPath)
+	{
+		try
+		{
+            Process proc = Process.Start(editor, entryBufferPath);
+            proc.WaitForExit();
+        }
+        catch (Exception e)
+		{
+            Console.Error.WriteLine($"Failed to start {editor}: {e.Message}");
+			return null;
+        }
+
+		using (FileStream fs = File.Open(entryBufferPath, FileMode.Open, FileAccess.Read, FileShare.None))
+		using (StreamReader sr = new(fs))
+		{
+			StringBuilder stringBuilder = new();
+			for (string? line = sr.ReadLine(); line != null && !line.StartsWith("//"); line = sr.ReadLine())
+			{
+				stringBuilder.AppendLine(line);
+			}
+			return stringBuilder.ToString();
+		}
+	}
+
+	private static void InsertEntry(string dataPath, string content)
+	{
+		using (SqliteConnection connection = new($"Data Source={dataPath}"))
+		{
+            connection.Open();
+
+            SqliteCommand command = connection.CreateCommand();
+
+            command.CommandText = 
+				@"CREATE TABLE IF NOT EXISTS entries (
+					id INTEGER PRIMARY KEY,
+					createdAt REAL NOT NULL,
+					content TEXT NOT NULL)";
+			command.ExecuteNonQuery();
+
+			command.CommandText =
+				@"INSERT INTO entries (createdAt, content)
+					VALUES ($createdAt, $content)";
+			command.Parameters.AddWithValue("$createdAt", DateTime.UtcNow);
+			command.Parameters.AddWithValue("$content", content);
+			command.ExecuteNonQuery();
+
+			connection.Close();
+        }
+	}
+}
---
+++ MicroJournal/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CommandLine;
+
+namespace MicroJournal;
+
+internal class Program
+{
+	private static string EDITOR_ENV_VAR = "MJ_EDITOR";
+	private static string DATA_ENV_VAR = "MJ_DATA";
+	private static string DEFAULT_DB_FILENAME = ".mj.db";
+	private static string WINDOWS_DEFAULT_EDITOR = "notepad.exe";
+	
+    private static async Task<int> Main(string[] args)
+    {
+		Option<string> editorOption = new(
+			aliases: new string[]{"-e", "--editor"},
+            description: "Path to the text editor to be used",
+			getDefaultValue: () =>
+			{
+				string? editorPath = Environment.GetEnvironmentVariable(EDITOR_ENV_VAR);
+				if (editorPath != null)
+					return editorPath;
+				return WINDOWS_DEFAULT_EDITOR;
+			});
+		
+		Option<string> dataOption = new(
+			aliases: new string[]{"-d", "--data"},
+			description: "Path to which entry data is stored",
+			getDefaultValue: () =>
+			{
+				string? dataPath = Environment.GetEnvironmentVariable(DATA_ENV_VAR);
+				if (dataPath != null)
+					return dataPath;
+				string homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+				return Path.Combine(homePath, DEFAULT_DB_FILENAME);
+			});
+		
+        RootCommand rootCommand = new("Command-line app for micro journaling");
+		rootCommand.AddGlobalOption(editorOption);
+		rootCommand.AddGlobalOption(dataOption);
+		
+		Command newEntryCommand = new("new", "Create a new entry");
+		newEntryCommand.SetHandler(App.CreateNewEntry, editorOption, dataOption);
+		rootCommand.AddCommand(newEntryCommand);
+		
+		return await rootCommand.InvokeAsync(args);
+    }
+}
